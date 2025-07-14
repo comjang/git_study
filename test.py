121241a4da8c7d4aee39731e9f31c6dce8aa61a6
홍길동 <hong12345@gmail.com>
2025-07-14 10:35:24 +0900

    2차

--- test.py
+++ test.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 import random as r
 x = []
 n = 10000
-m = 5
+m = 3
 for _ in range(n):
   sum = 0
   for _ in range(m):
     sum += r.randrange(1, 7)
   x.append(sum)
-plt.hist(x, bins=5*m)
+plt.hist(x, bins=3*m)
 plt.show()
